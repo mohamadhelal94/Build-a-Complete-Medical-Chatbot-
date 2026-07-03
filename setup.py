d2d9dbb8ce0b42cb95f9151a3929350de5b40e2b
Mohamad Abou Helal <mhd.salah94@gmail.com>
2026-07-03 13:48:21 +0200

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setupools import setup, find_packages,setup
+
+setup(
+    name="medical-chatbot",
+    version="0.1.0",
+    author="Mohamad Abou Helal",
+    author_email="mhd.salah94@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
